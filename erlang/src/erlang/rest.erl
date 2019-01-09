45507501333728cf4d53a3230084e1a6c135bc67
Joaquim <joaquim.mgo.97@gmail.com>
2019-01-09 18:51:47 +0000

    Remove files

---
+++ erlang/src/erlang/rest.erl
@@ -0,0 +1,10 @@
+-module(rest).
+
+-export([ getUsers/0, registerUser/4 ]).
+
+
+getUsers()-> #{ "quim" => {0,"pass", investor}, 
+                "pedro" => {1, "pass", company}
+              }.
+
+registerUser( Username, Passwd, Type, Id  )-> ok.
